aaff137f47a73cc446a1c0d9953af45714e0b1db
ngoi1minh15 <80388176+ngoi1minh15@users.noreply.github.com>
2021-07-03 15:54:54 +0700

    Add new files

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin chào cô gái!',
+    introDesc: 'WELCOME TO MY HEART',
+    btnIntro: 'Bấm vào đây nè cậu',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích cậu <33',
+    btnNo: 'Không, chúng ta chỉ là bạn :3',
+    question:'Trên thế giới có hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều lắm 😘😘',
+    messDesc: 'Tối mai 7h, anh qua đón nhé công chúa.',
+    btnAccept: 'Okieee lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100034488072982' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
